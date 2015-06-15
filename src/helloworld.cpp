9cd783563d67d49afdf559ad06358f7bfc78cd8a
hjweddie <hjweddie@163.com>
2015-06-15 20:39:40 +0800

    init

---
+++ src/helloworld.cpp
@@ -0,0 +1,16 @@
+/*
+*  Author:   suyuhui@youmi.net
+*            huangjunwei@youmi.net
+*  Time:     Mon 15 Jun 2015 08:26:12 PM CST
+*  File:     helloworld.cpp
+*  Desc:     just 4 test
+*/
+
+#include <iostream>
+#include "helloworld.h"
+
+using namespace std;
+
+void hello_world() {
+	cout << "Hello World!!" << endl;
+}
---
+++ src/q++.cpp
@@ -0,0 +1,17 @@
+/*
+*  Author:   suyuhui@youmi.net
+*  	     huangjunwei@youmi.net
+*  Time:     Mon 15 Jun 2015 08:19:50 PM CST
+*  File:     q++.cpp
+*  Desc:     main function of this project
+*/
+
+
+#include "helloworld.h"
+
+
+int main(int n, char **args) {
+	hello_world();
+	return 0;
+}
+
